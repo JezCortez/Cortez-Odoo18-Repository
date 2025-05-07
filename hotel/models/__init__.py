f8f8186353ddac403e365a8145a3dd82af7e52a2
Jez Cortez <74903241+JezCortez@users.noreply.github.com>
2025-05-07 15:03:06 +0800

    Delete hotel directory

--- hotel/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import charges
-from . import roomtypes
-from . import rooms
-from . import guests
-from . import guestregistration
--- hotel/sql/idx_reg_schedule_guestregistration.sql
+++
@@ -1,2 +0,0 @@
-CREATE INDEX IF NOT EXISTS idx_reg_schedule_guestregistration
-ON hotel_guestregistration (room_id, state);
